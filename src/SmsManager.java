dba2c4fdbad448cee1da11ecdb2acf61612df61e
camber01 <bernabe.cambarihan@gcash.com>
2022-02-18 12:13:10 +0800

    Implements SmsManagerInterface

---
+++ src/SmsManager.java
@@ -0,0 +1,37 @@
+public class SmsManager implements SmsManagerInterface {
+
+    @Override
+    public String insert_sms() {
+        return null;
+    }
+
+    @Override
+    public String getSmsByDate() {
+        return null;
+    }
+
+    @Override
+    public String getSmsByPromoCode() {
+        return null;
+    }
+
+    @Override
+    public String getSmsByMsisdn(String msisdn) {
+        return null;
+    }
+
+    @Override
+    public String getSmsBySent() {
+        return null;
+    }
+
+    @Override
+    public String getSmsByReceive() {
+        return null;
+    }
+
+    @Override
+    public String getSmsByMsisdn(String[] msisdn) {
+        return null;
+    }
+}
